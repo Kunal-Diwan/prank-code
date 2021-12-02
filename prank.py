e70beb678ee4617d4306b5a7b7133281c124db36
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-12-02 17:41:32 +0530

    Create prank.py

---
+++ prank.py
@@ -0,0 +1,23 @@
+name=input("what's your name ??")
+x=name.title()
+print(f"#1.Your name is \t{x} ")
+print('_______________________')
+y=len(x)
+print(" 🐲 🐲 🐲 🐲 🐲 🐲 🐲 ")
+print(f"#2.length of your name is {y}")
+print("#########################")
+print(f"#3.hahhaha can you imagine\n reverse of your name is {x[-1::-1]}")
+age =input("What's your age? \n==> ")
+age=int(age)
+if age>=18:
+	print("you are 18+ bro 😎 😎 😎")
+else:
+	print("you are kid ..\n..take horlicks 👺 👺 👺 👺 👺 👺")
+print('Don\'t believe a stranger 👽 👽 👽 👽 👉🏻 👉🏻 ')
+x=input('will you believe me?? \nyes/no   ::>')
+if x=='no':
+	while True:
+		print("you are hacked 🐲 🕥 🐲")
+		print("your system is cracked 🐲 🐲 🐲")
+elif x=='yes':
+	print('you are safe dude .\n..wanna friend with me ??\nthen my instagram profile=> ✓✓✓✓✓https://www.instagram.com/kunaldiwan1405')
